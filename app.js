94a4ef185947d46977f31d86f9cc6742430a18aa
Rafe Uddaraj <78890044+rafeuddaraj@users.noreply.github.com>
2021-09-10 20:58:49 +0600

    Functional Problem

--- app.js
+++ app.js
@@ -29,16 +29,6 @@ const createElement = function (text, number) {
   return listItem;
 };
 
-const shuffle = function (arr) {
-  const shuffleArr = [...arr]
-  for (let i = shuffleArr.length - 1; i > 0; i--){
-    const rand = Math.floor(Math.random() * (i + 1))
-    const temp = shuffleArr[rand]
-    shuffleArr[rand] = shuffleArr[i]
-    shuffleArr[i] = temp
-  }
-  return shuffleArr
-}
 
 allName.addEventListener('keypress', function (e) {
   if (e.key === 'Enter') {
@@ -59,36 +49,116 @@ allName.addEventListener('keypress', function (e) {
   }
 });
 
-giveATryBtn.addEventListener('click', function () {
-  if (participant.length <= 0) {
-    alert('Please Provide Some Students Names');
-  } else {
-    let shuffleArr = shuffle(participant)
-    for (let i = 0; i < shuffleArr.length; i++){
-      setTimeout(() => {
-        
-        display.innerHTML = shuffleArr[i]
 
-        if (shuffleArr.length - 1 === i) {
-          
+const shuffle = function (arr) {
+  const shuffleArr = [...arr];
+  for (let i = shuffleArr.length - 1; i > 0; i--) {
+    const rand = Math.floor(Math.random() * (i + 1));
+    const temp = shuffleArr[rand];
+    shuffleArr[rand] = shuffleArr[i];
+    shuffleArr[i] = temp;
+  }
+  return shuffleArr;
+};
+
+giveATryBtn.addEventListener('click', function () {
+  if (participant.length === 0) {
+    alert('Please Provide Some Students ');
+  } else {
+    const shuffleName = shuffle(participant);
+    console.log(shuffleName);
+    shuffleName.forEach((names, index) => {
+      setTimeout(() => {
+        display.innerHTML = shuffleName[index];
+
+        if (shuffleName.length - 1 === index) {
+
           if (!firstWinner.innerHTML) {
-            firstWinner.innerHTML = shuffleArr[i]
-            participant.splice(shuffleArr[i],1)
-          }else if (!secondWinner.innerHTML) {
-            secondWinner.innerHTML = shuffleArr[i]
-            participant.splice(shuffleArr[i],1)
-          } else if (!thirdWinner.innerHTML) {
-            thirdWinner.innerHTML = shuffleArr[i]
-            participant.splice(shuffleArr[i], 1)
-            display.innerHTML = 'Congratulations'
-            this.remove()
+            const ind = participant.indexOf(shuffleName[index]);
+            firstWinner.innerHTML = shuffleName[index];
+            participant.splice(ind, 1);
+          }
+          else if (!secondWinner.innerHTML) {
+            const ind = participant.indexOf(shuffleName[index]);
+            secondWinner.innerHTML = shuffleName[index];
+            participant.splice(ind, 1);
+          }
+          else if (!thirdWinner.innerHTML) {
+            const ind = participant.indexOf(shuffleName[index]);
+            thirdWinner.innerHTML = shuffleName[index];
+            participant.splice(ind, 1);
           }
 
         }
-
-      },i*100)
-    }
+      }, index * 100);
+    });
   }
+  console.log(participant);
 });
 
+// const nameCollection = function (event) {
+//   if (event.key === 'Enter') {
+//     if (this.value === '' || this.value === '\n') {
+//       alert('Please Provide Some Students Names')
+//     } else {
+//       const names = event.target.value.split(', ')
+//       this.value = ''
+//       nameStore(names,participant,studentList,studentListShow)
+//     }
+//   }
+// }
 
+
+// const nameStore = function (names,participantNames,listShow,showBtn) {
+//   const name = names
+//   participantNames.push(...name)
+//   participantNames.forEach(participantName => {
+//     const listItem = createElement(participantName)
+//     showBtn.onclick = studentsListShow(listShow,listItem)
+//   })
+// }
+
+// const studentsListShow = function (listShow, listItem) {
+//   listShow.appendChild(listItem)
+// }
+
+
+
+// const giveATryAction = function () {
+//   if (participant.length === 0) {
+//     alert('Please Provide Some Students')
+//   } else {
+//     const shuffleArr = shuffle(participant)
+//     shuffleArr.forEach((names, index, arr) => {
+//       setTimeout(()=>displayBind(index, arr, display), 100 * index)
+//     })
+//   }
+// }
+
+
+// const displayBind = function (index,arr,display) {
+//   display.innerHTML = arr[index]
+//   if (firstWinner) {
+//     winner(index,arr,firstWinner.innerHTML)
+//   }
+//   else if (secondWinner) {
+//     winner(index,arr,secondWinner.innerHTML)
+//   }
+//   else if (thirdWinner) {
+//     winner(index,arr,thirdWinner.innerHTML)
+//   }
+// }
+
+// const winner = function (index,arr,condition) {
+//   if (!condition) {
+//     condition = arr[index]
+//     const ind = participant[arr[index]]
+//     participant.splice(ind)
+//   } 
+// }
+
+
+
+
+// allName.addEventListener('keypress', nameCollection);
+// giveATryBtn.addEventListener('click',giveATryAction)
